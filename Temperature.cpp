5ea01a223b58340d105978660c0bc98aa65cb8d8
evaristocuesta <evaristocg@gmail.com>
2023-01-05 18:13:50 +0100

    Add Temperature.cpp

---
+++ Temperature.cpp
@@ -0,0 +1,37 @@
+#include "Temperature.h"
+
+float Temperature::temperature() 
+{
+    _sensors.requestTemperaturesByIndex(0);
+    float temperatureC = _sensors.getTempCByIndex(0);
+    return temperatureC;
+}
+
+Temperature::Temperature(unsigned long interval, int oneWireBus) 
+{
+    _interval = interval;
+    _oneWire = OneWire(oneWireBus);
+    _sensors = DallasTemperature(&_oneWire);
+}
+
+void Temperature::setup() 
+{
+    _sensors.begin();
+    _temperature = temperature();
+}
+
+void Temperature::update() 
+{
+    unsigned long currentMillis = millis();
+
+    if ((unsigned long)(currentMillis - _previousMillis) >= _interval) 
+    {
+        _temperature = temperature();
+        _previousMillis = currentMillis;
+    }
+}
+
+float Temperature::getTemperature() 
+{
+    return _temperature;
+}
--- Temperature.h
+++ Temperature.h
@@ -11,36 +11,13 @@ class Temperature {
     float _temperature;
     unsigned long _interval;
 
-    float temperature() {
-      _sensors.requestTemperaturesByIndex(0);
-      float temperatureC = _sensors.getTempCByIndex(0);
-      return temperatureC;
-    }
+    float temperature();
 
   public: 
-    Temperature(unsigned long interval, int oneWireBus) {
-      _interval = interval;
-      _oneWire = OneWire(oneWireBus);
-      _sensors = DallasTemperature(&_oneWire);
-    }
-
-    void setup() {
-      _sensors.begin();
-      _temperature = temperature();
-    }
-    
-    void update() {
-      unsigned long currentMillis = millis();
-      
-      if ((unsigned long)(currentMillis - _previousMillis) >= _interval) {
-        _temperature = temperature();
-        _previousMillis = currentMillis;
-      }
-    }
-
-    float getTemperature() {
-      return _temperature;
-    }
+    Temperature(unsigned long interval, int oneWireBus);
+    void setup();
+    void update();
+    float getTemperature();
 };
 
 #endif
